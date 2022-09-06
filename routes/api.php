e04ba648c9ac2d0c75810881c052f93ef45cab64
Thomas Effersøe <thoeff@clio.me>
2022-09-06 13:10:58 +0200

    changed api/v1/ingredient endpoint

--- routes/api.php
+++ routes/api.php
@@ -20,5 +20,5 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 Route::get('/v1/recipes', [\App\Http\Controllers\Api\RecipeController::class, 'index']);
 Route::get('/v1/recipes/{id}', [\App\Http\Controllers\Api\RecipeController::class, 'show']);
-Route::get('/v1/ingredient', [\App\Http\Controllers\Api\IngredientController::class, 'index']);
+Route::get('/v1/ingredients', [\App\Http\Controllers\Api\IngredientController::class, 'index']);
 Route::post('/v1/scrape', [\App\Http\Controllers\Api\ScrapeController::class, 'index'])->middleware('recipe.form.validate')->name('api.scrape');
